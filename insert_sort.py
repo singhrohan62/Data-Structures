4c45c705d2f1bcfd139633ebf2fe9244e7a36efd
Rohan Singh <rohaniiitv@gmail.com>
2019-06-21 21:24:37 +0530

    added python prg for insert sort

---
+++ insert_sort.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+__author__ = 'Rohan Singh'
+
+def insertion_sort(list):
+	for x in range(1,len(list)):
+		key = list[x]
+		j = x-1
+		while j >= 0 and list[j] > key:
+			#swap
+			temp = key
+			list[j+1] = list[j]
+			list[j] = key
+			j-=1
+
+def main():
+	num = int(input("Enter size of array: "))
+	print(num)
+	list = np.random.randint(1, 10000, num)
+	print(list)
+	insertion_sort(list)
+	print(list)
+
+if __name__ == '__main__':
+	main()
